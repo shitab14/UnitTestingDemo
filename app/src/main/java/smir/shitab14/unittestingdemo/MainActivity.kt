f41eac2752f172c663acfe263542be1b0449df67
Shitab Mir <mushfiq.islam@hungrynaki.com>
2022-10-12 10:32:53 +0600

    Added View Binding

--- app/src/main/java/smir/shitab14/unittestingdemo/MainActivity.kt
+++ app/src/main/java/smir/shitab14/unittestingdemo/MainActivity.kt
@@ -1,11 +1,20 @@
 package smir.shitab14.unittestingdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import smir.shitab14.unittestingdemo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
     }
 }
