f13a5f44822535ee7a45f520af2e760a81c2938d
Shitab Mir <mushfiq.islam@hungrynaki.com>
2022-10-12 11:45:22 +0600

    Added View (Unimportant)

--- app/src/main/java/smir/shitab14/unittestingdemo/MainActivity.kt
+++ app/src/main/java/smir/shitab14/unittestingdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package smir.shitab14.unittestingdemo
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import smir.shitab14.unittestingdemo.databinding.ActivityMainBinding
 
 
@@ -16,5 +17,31 @@ class MainActivity : AppCompatActivity() {
         val view: View = binding.root
         setContentView(view)
 //        setContentView(R.layout.activity_main)
+
+        setOnClickListeners()
     }
+
+    private fun setOnClickListeners() {
+        binding.btnToast.setOnClickListener {
+            val toast = getReturnSomethingObject().returnsAToast( "Button Clicked")
+            toast.show()
+        }
+        binding.btnSnackBar.setOnClickListener {
+            Snackbar.make(binding.clRootLayout, "This is a Snack Bar", Snackbar.LENGTH_LONG).show()
+        }
+        binding.btnOpenDialog.setOnClickListener {
+//            Toast.makeText(this, "This is a Toast", Toast.LENGTH_LONG).show()
+        }
+        binding.btnBottomSheet.setOnClickListener {
+//            Toast.makeText(this, "This is a Toast", Toast.LENGTH_LONG).show()
+        }
+        binding.btnAPICall.setOnClickListener {
+//            Toast.makeText(this, "This is a Toast", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getReturnSomethingObject(): ReturnsSomethingRequiresContext {
+        return ReturnsSomethingRequiresContext(this)
+    }
+
 }
